1551e883ce7f54a60817e426bb1e06e3e9e9a1d1
Rajesh Alli <arajesh.alli@gmail.com>
2022-05-06 12:50:02 +0530

    changes

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,7 +8,6 @@ Print "Installing Nginx"
 yum install nginx -y &>>$LOG
 Stat $?
 
-
 Print "Download Html Pages"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 Stat $?
@@ -18,7 +17,7 @@ rm -rf /usr/share/nginx/html/* &>>$LOG
 Stat $?
 
 Print "Extract Frontend Archive"
-unzip -d /tmp /tmp/frontend.zip &>>$LOG
+unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
 Stat $?
 
 Print "Copy files to Nginx path"
@@ -29,9 +28,9 @@ Print "Copy Nginx Roboshop Config file"
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
 Stat $?
 
-Print "Update Nginx Config file"
-sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/'  -e '/user/ s/localhost/user.roboshop.internal/'  -e '/payment/ s/localhost/payment.roboshop.internal/'  -e '/shipping/ s/localhost/shipping.roboshop.internal/'   /etc/nginx/default.d/roboshop.conf  &>>$LOG
-Stat $?
+#Print "Update Nginx Config file"
+#sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/'  -e '/user/ s/localhost/user.roboshop.internal/'  -e '/payment/ s/localhost/payment.roboshop.internal/'  -e '/shipping/ s/localhost/shipping.roboshop.internal/'   /etc/nginx/default.d/roboshop.conf  &>>$LOG
+#Stat $?
 
 Print "Enabling Nginx"
 systemctl enable nginx &>>$LOG
@@ -40,4 +39,3 @@ Stat $?
 Print "Starting Nginx"
 systemctl restart nginx &>>$LOG
 Stat $?
-
