533f5e3324bd7c142739e086a48fb51bb01ffef5
Rajesh Alli <arajesh.alli@gmail.com>
2022-05-06 15:20:37 +0530

    changes

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
-source components/common.sh
+Print "install NodeJS"
+yum install nodejs make gcc-c++ -y &>>$LOG
+Stat $?
 
-MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
+Print "Add RoboShop User"
+id roboshop &>>$LOG
+if [ $? -eq 0 ]; then
+  echo User RoboShop already exists &>>$LOG
+else
+  useradd roboshop  &>>$LOG
+fi
+Stat $?
 
- Print "Install NodeJS dependencies"
-  cd /home/roboshop/${COMPONENT}
-  npm install --unsafe-perm &>>$LOG
-  Stat $?
+Print "Download Catalogue"
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+Stat $?
+
+Print "Remove Old Content"
+rm -rf /home/roboshop/catalogue &>>$LOG
+Stat $?
+
+Print "Extract Catalogue"
+unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
+Stat $?
+
+Print "copy content"
+mv /home/roboshop/catalogue-main /home/roboshop/catalogue &>>$LOG
+Stat $?
+
+Print "Install NodeJS Dependencies"
+$ npm install --unsafe-perm &>>$LOG
+Stat $?
 
 
-COMPONENT_NAME=Catalogue
-COMPONENT=catalogue
-NODEJS
-CHECK_MONGO_FROM_APP
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
