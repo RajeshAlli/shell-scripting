a442d33fe7058219c72da1eacf47d615d57c1b96
Rajesh Alli <arajesh.alli@gmail.com>
2022-05-06 13:39:38 +0530

    changes

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,6 +4,11 @@ source components/common.sh
 
 MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
+ Print "Install NodeJS dependencies"
+  cd /home/roboshop/${COMPONENT}
+  npm install --unsafe-perm &>>$LOG
+  Stat $?
+
 
 COMPONENT_NAME=Catalogue
 COMPONENT=catalogue
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,7 +25,7 @@ mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
 Stat $?
 
 Print "Copy Nginx Roboshop Config file"
-cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf  &>>$LOG
+cp /tmp/frontend-main/localhost.conf root  &>>$LOG
 Stat $?
 
 #Print "Update Nginx Config file"
