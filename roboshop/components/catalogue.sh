db6ce46a1e9d25c285725365fe8f898d61949657
Rajesh Alli <arajesh.alli@gmail.com>
2022-05-10 10:09:26 +0530

    ss

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -40,4 +40,4 @@ Stat $?
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
 # systemctl start catalogue
-# systemctl enable catalogue
+# systemctl enable cataloguels
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!git /bin/bash
 
 source components/common.sh
 MSPACE=$(cat $0 | grep Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
