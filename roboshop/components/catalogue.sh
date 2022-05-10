7d72a10e3fee1ed5aa45f992b71f9d9d262dddb9
Rajesh Alli <arajesh.alli@gmail.com>
2022-05-10 10:28:37 +0530

    ss

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -36,23 +36,22 @@ cd /home/roboshop/catalogue
 npm run ng --version --unsafe-perm &>>$LOG
 Stat $?
 
-SYSTEMD() {
-  Print "Fix App Permissions"
-  chown -R roboshop:roboshop /home/roboshop
-  Stat $?
+Print "Fix App Permissions"
+Chown -R roboshop:roboshop /home/roboshop
+Stat $?
 
-  Print "Update DNS records in SystemD config"
-  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service   &>>$LOG
-  Stat $?
+Print "Update DNS records in SystemD config"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service   &>>$LOG
+Stat $?
 
-  Print "Copy SystemD file"
-  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-  Stat $?
+Print "Copy SystemD file"
+mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+Stat $?
 
-  Print "Start ${COMPONENT_NAME} Service"
-  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
-  Stat $?
-}
+Print "Start ${COMPONENT_NAME} Service"
+systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+Stat $?
+
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
