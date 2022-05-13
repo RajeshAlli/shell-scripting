b4d73b9fcfae75c185afc2a65e9496792025cb6b
Rajesh Alli <arajesh.alli@gmail.com>
2022-05-13 15:51:14 +0530

    ss

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,58 +2,10 @@
 
 source components/common.sh
 
-Print "install NodeJS"
-yum install nodejs make gcc-c++ -y &>>$LOG
-Stat $?
-
-Print "Add RoboShop User"
-id roboshop &>>$LOG
-if [ $? -eq 0 ]; then
-  echo User RoboShop already exists &>>$LOG
-else
-  useradd roboshop  &>>$LOG
-fi
-Stat $?
-
-Print "Download Catalogue"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
-Stat $?
-
-Print "Remove Old Content"
-rm -rf /home/roboshop/catalogue &>>$LOG
-Stat $?
-
-Print "Extract Catalogue"
-unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
-Stat $?
-
-Print "copy content"
-mv /home/roboshop/catalogue-main /home/roboshop/catalogue &>>$LOG
-Stat $?
-
-Print "Install NodeJS Dependencies"
-cd /home/roboshop/catalogue
-npm install --unsafe-perm &>>$LOG
-Stat $?
-
-Print "Fix App Permissions"
-chown -R roboshop:roboshop /home/roboshop
-Stat $?
-
-Print "Update DNS records in SystemD config"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service  &>>$LOG
-Stat $?
-
-Print "Copy SystemD file"
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-Stat $?
-
-Print "Start catalogue Service"
-systemctl daemon-reload &>>$LOG && systemctl restart catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
-Stat $?
+MSPACE=$(cat $0 components/common.sh | grep ^Print | awk -F '"' '{print $2}' | awk '{ print length }' | sort | tail -1)
 
 
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# systemctl daemon-reload
-# systemctl start catalogue
-# systemctl enable cataloguels
+COMPONENT_NAME=Catalogue
+COMPONENT=catalogue
+NODEJS
+CHECK_MONGO_FROM_APP
